8ebced52a04a505ac2497fa40700044a1faa88b4
xuyu <xypisces@163.com>
2018-07-10 18:04:34 +0800

    puppeteer

---
+++ puppeteer/app.js
@@ -0,0 +1,18 @@
+const puppeteer = require('puppeteer');
+
+async function sleep(){
+  await setTimeout(()=>{
+    console.log('wait')
+  },3000)
+}
+
+(async () =>{
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto('https://mp.weixin.qq.com/')
+  await sleep()
+  await page.screenshot({path: 'new.png'})
+  await page.pdf({path: 'a.pdf', format: 'A4'})
+  console.log('success~')
+  await browser.close();
+})();
