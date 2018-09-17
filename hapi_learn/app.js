bd55440dcfb32abd94d0bdeb05fe6488652340b1
xypisces <xypisces@163.com>
2018-09-17 22:46:28 +0800

    'joi'

--- hapi_learn/app.js
+++ hapi_learn/app.js
@@ -1,7 +1,7 @@
 const Hapi = require('hapi')
 const config = require('./config/index')
 const routeApi = require('./routes/index')
-
+const pluginHapiSwagger = require('./plugins/hapi-swagger');
 const server = new Hapi.Server()
 
 server.connection({
@@ -10,7 +10,11 @@ server.connection({
 })
 
 const init = async () => {
-  server.route(routeApi)
+  await server.register([
+    // 为系统使用 hapi-swagger
+    ...pluginHapiSwagger,
+  ]);
+  server.route([...routeApi])
   await server.start()
   console.log(`server is running at ${server.info.uri}`)
 }
