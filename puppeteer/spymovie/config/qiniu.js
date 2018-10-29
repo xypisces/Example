1251ec944f10e347b75bdd4e7e4beea10ad269c1
xuyu <xypisces@163.com>
2018-10-29 20:29:47 +0800

    config

--- puppeteer/spymovie/config/qiniu.js
+++ puppeteer/spymovie/config/qiniu.js
@@ -1,7 +1,7 @@
 module.exports = {
   "qiniu": {
     'bucket': 'moviepic',
-    'AK': 't7--alpd6s8tU_nw-CPr-zDleM4F8p3DkrXWXR9a',
-    'SK': 'nsTaWkTYBHsTsJ9vztwdu-erNk1YQSddIHi5TrqA',
+    'AK': '1t7--alpd6s8tU_nw-CPr-zDleM4F8p3DkrXWXR9a',
+    'SK': '1nsTaWkTYBHsTsJ9vztwdu-erNk1YQSddIHi5TrqA',
   }
 }
